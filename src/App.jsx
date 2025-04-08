f715786f49b797824dd34e11621bfe4b455e183d
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-04-08 16:21:35 +0200

    feat: add Header and Main in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import Header from './components/Header';
+import Main from './components/Main';
+
 function App() {
   return (
-    <></>
+    <>
+      <Header />
+      <Main />
+    </ >
   );
 }
 
